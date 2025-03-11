fd1e57acc90cd8b0c32ecf1281614266514fb963
Nate Z <ntzeitlin@gmail.com>
2025-03-11 09:57:11 -0500

    Client can now GET user details from server

--- json-server.py
+++ json-server.py
@@ -15,6 +15,7 @@ from views import (
     get_current_user_posts,
     delete_post,
     get_all_users,
+    get_user_detail,
 )
 
 
@@ -47,6 +48,9 @@ class JSONServer(HandleRequests):
                 "", status.HTTP_400_CLIENT_ERROR_BAD_REQUEST_DATA.value
             )
         elif url["requested_resource"] == "users":
+            if url["pk"] != 0:
+                response_body = get_user_detail(url["pk"])
+                return self.response(response_body, status.HTTP_200_SUCCESS.value)
             response_body = get_all_users()
             return self.response(response_body, status.HTTP_200_SUCCESS.value)
         else:
