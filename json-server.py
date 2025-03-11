ef600e0b579b1ea8505d343ace61fee7106043c3
Nate Z <ntzeitlin@gmail.com>
2025-03-11 14:11:22 -0500

    added json parsing to response return for subscriptions

--- json-server.py
+++ json-server.py
@@ -123,7 +123,9 @@ class JSONServer(HandleRequests):
             return self.response("", status.HTTP_201_SUCCESS_CREATED.value)
         elif url["requested_resource"] == "subscriptions":
             new_item = create_subscription(parsed_body)
-            return self.response(new_item, status.HTTP_201_SUCCESS_CREATED.value)
+            return self.response(
+                json.dumps(new_item), status.HTTP_201_SUCCESS_CREATED.value
+            )
         else:
             return self.response(
                 "Invalid resource",
