a6277ab44c7a950fb5264898c727848f5762956e
Niles Clancy <nilesclancy@gmail.com>
2025-03-13 13:20:35 -0400

    changed allpost view

--- views/post.py
+++ views/post.py
@@ -1,14 +1,13 @@
 import sqlite3
 import json
 
-# All posts should include post title, author name and category and date added.
+# All posts should include post title, author name, category and date added.
 
 def view_all_posts(user_id=None):
     with sqlite3.connect("./db.sqlite3") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
-        # // base SQL query
         query = """
         SELECT
             p.id, 
@@ -37,45 +36,6 @@ def view_all_posts(user_id=None):
         posts = [dict(row) for row in query_results]
 
         return json.dumps(posts)
-
-# def view_all_posts():
-#     with sqlite3.connect("./db.sqlite3") as conn:
-#         conn.row_factory = sqlite3.Row
-#         db_cursor = conn.cursor()
-
-#         db_cursor.execute(
-#             """
-#         SELECT
-#             p.id, 
-#             p.title,
-#             p.publication_date, 
-#             p.category_id,
-#             p.user_id,
-#             c.label AS category_label,
-#             u.first_name,
-#             u.last_name
-#         FROM Posts AS p
-#         JOIN Categories c ON c.id = p.category_id
-#         JOIN Users u ON u.id = p.user_id          
-#         """
-#         )
-#         # Retrieve Results:
-#         query_results = db_cursor.fetchall()
-
-#         posts = []
-
-#         for row in query_results:
-#             posts.append(dict(row))
-
-#         sorted_results = sorted(
-#             posts, key=lambda post: post["publication_date"], reverse=True
-#         )
-#         serialized_results = json.dumps(sorted_results) if posts else {}
-#     return serialized_results
-
-
-# sorted_data = sorted(data, key=lambda x: x["name"])
-
 
 def view_post_detail(pk):
     with sqlite3.connect("./db.sqlite3") as conn:
