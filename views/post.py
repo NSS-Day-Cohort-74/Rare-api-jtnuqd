0dde07b9e7108913054be71881a09a23186000aa
Nate Z <assured.toady_0n@icloud.com>
2025-03-14 13:34:36 -0500

    Revert "Ft/filter by user"

--- views/post.py
+++ views/post.py
@@ -1,14 +1,16 @@
 import sqlite3
 import json
 
-# All posts should include post title, author name, category and date added.
+# All posts should include post title, author name and category and date added.
 
-def view_all_posts(user_id=None):
+
+def view_all_posts():
     with sqlite3.connect("./db.sqlite3") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
-        query = """
+        db_cursor.execute(
+            """
         SELECT
             p.id, 
             p.title,
@@ -20,22 +22,26 @@ def view_all_posts(user_id=None):
             u.last_name
         FROM Posts AS p
         JOIN Categories c ON c.id = p.category_id
-        JOIN Users u ON u.id = p.user_id
+        JOIN Users u ON u.id = p.user_id          
         """
-
-        params = ()
-        if user_id:  
-            query += " WHERE p.user_id = ?"
-            params = (user_id,)
-
-        query += " ORDER BY p.publication_date DESC"  # // sorting by most recent first
-
-        db_cursor.execute(query, params)
+        )
+        # Retrieve Results:
         query_results = db_cursor.fetchall()
 
-        posts = [dict(row) for row in query_results]
+        posts = []
 
-        return json.dumps(posts)
+        for row in query_results:
+            posts.append(dict(row))
+
+        sorted_results = sorted(
+            posts, key=lambda post: post["publication_date"], reverse=True
+        )
+        serialized_results = json.dumps(sorted_results) if posts else {}
+    return serialized_results
+
+
+# sorted_data = sorted(data, key=lambda x: x["name"])
+
 
 def view_post_detail(pk):
     with sqlite3.connect("./db.sqlite3") as conn:
